073ddb0de75a15d971fba8417bd445a7e85be96f
NaviD <NaviD@DESKTOP-ILMPI5P>
2020-05-30 02:44:20 +0430

    CacheRedis

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : ApiController
+    {
+        public string GetNavid()
+        {
+            return "Welcome to WebApi";
+        }
+        public List<string> GetNavid(int Id)
+        {
+            return new List<string>
+            {
+                "Data1","Data2"
+            };
+        }
+    }
+}
---
+++ WebApplication1/Interface/IResponseCacheService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Interface
+{
+    interface IResponseCacheService
+    {
+        Task CacheResponseAsync(string cachekey, object response, TimeSpan timeToLive);
+        Task<string> GetCachedResponseAsync(string cacheKey);
+    }
+}
