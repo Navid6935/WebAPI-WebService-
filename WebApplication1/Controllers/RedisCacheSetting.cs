0c9608f8914b3d1ced09ffa5561a4c73a03ffa5c
NaviD6935 <navid.shatel@gmail.com>
2020-05-30 17:10:05 +0430

    RedisCache_990310

---
+++ WebApplication1/Controllers/RedisCacheSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class RedisCacheSetting
+    {
+        public bool Enabled { get; set; }
+        public string ConnectionString { get; set; }
+        public int DefaultSecondsToCache { get; set; }
+    }
+    public class CacheInstaller :IServiceInstaller
+    {
+
+    }
+}
---
+++ WebApplication1/Controllers/ResponceCacheService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebApplication1.Interface;
+
+namespace WebApplication1.Controllers
+{
+    public class ResponceCacheService:IResponseCacheService
+    {
+        private readonly IDistributedCache _distributedCache;
+            public ResponceCacheService(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public async Task CacheResponseAsync(string cachekey, object response, TimeSpan timeToLive)
+        {
+            if (response == null) return;
+            
+                var serializeResponce = JsonConvert.SerializeObject(response);
+            await _distributedCache.SetStringAsync(cachekey, serializeResponce, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = timeToLive
+            });
+                    
+            
+        }
+
+        public async Task<string> GetCachedResponseAsync(string cacheKey)
+        {
+            var cachedResponse = await _distributedCache.GetStringAsync(cacheKey);
+            return string.IsNullOrWhiteSpace(cachedResponse) ? null : cachedResponse;
+        }
+
+    }
+}
